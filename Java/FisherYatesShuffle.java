28fb2d60dc08746a38a89465a210a4c488bfb4aa
jayesh-acl <jayesh.das@acldigital.com>
2024-10-04 10:47:39 +0530

    Update FisherYatesShuffle.java

--- Java/FisherYatesShuffle.java
+++ Java/FisherYatesShuffle.java
@@ -5,15 +5,15 @@ public class FisherYatesShuffle {
     public static int[] shuffle(int[] array) {
         Random random = new Random();
 
-        for (int i = 0; i < array.length; i++) {
-            int place = random.nextInt(i+1);
+        // for (int i = 0; i < array.length; i++) {
+        //     int place = random.nextInt(i+1);
 
-            int current = array[i];
+        //     int current = array[i];
 
-            int temporaryNumber = array[place];
-            array[place] = array[i];
-            array[i] = temporaryNumber;
-        }
+        //     int temporaryNumber = array[place];
+        //     array[place] = array[i];
+        //     array[i] = temporaryNumber;
+        // }
         return array;
     }
     // Main Function
@@ -25,11 +25,11 @@ public class FisherYatesShuffle {
         }
     }
 
-    /*
-    Sample I/O:
+    // /*
+    // Sample I/O:
 
-    Input: 1, 2, 3, 4, 5
-    Output: 3, 4, 1, 5, 2
+    // Input: 1, 2, 3, 4, 5
+    // Output: 3, 4, 1, 5, 2
 
     Time Complexity: O(n)
     Space Complexity: O(n)
